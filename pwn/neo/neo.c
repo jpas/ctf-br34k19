3566144a68f01593539ef2da02192f3a3fa56806
Jarrod Pas <jarrod@jarrodpas.com>
2019-02-13 10:31:16 -0600

    Adds one_gadget

---
+++ pwn/neo/neo.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int learn() {
+    char skill[64];
+    gets(skill);
+    return skill[0] == '\0';
+}
+
+int main() {
+    do {
+        puts("What would you like to learn?");
+    } while (learn());
+}
